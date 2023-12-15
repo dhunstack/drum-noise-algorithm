ee81d072c5bdbf41a6daea8e7f0d71f78176b011
Anmol Mishra <anmolmishra1997@gmail.com>
2023-12-15 13:17:37 +0100

    [feat] Add new curve type and keys sorting
    Add support for new curve type, exponential function with exponential release
    Modify parser to sort keys before storing in json
    
    Signed-off-by: Anmol Mishra <anmolmishra1997@gmail.com>

--- Curves.py
+++ Curves.py
@@ -2,3 +2,11 @@ import numpy as np
 
 def exp(x, A, B):
     return np.exp(-A*x) * (1 - np.exp(-B * x))
+
+def exprel(x, A, B, C):
+    relT = np.argmax(x >= C)
+        
+    r1 =  np.exp(-A * x[0:relT]) * (1 - np.exp(-B * x[0:relT]))
+    r2 = np.exp(-A * C) * (1 - np.exp(-B * C)) * np.exp(-(x[relT:] - C))
+
+    return np.append(r1, r2)
--- Parser.py
+++ Parser.py
@@ -19,8 +19,6 @@ def write_params_to_json_file(id, length, osc_freqs, osc_bank_gains, noise_bank_
     """
     with open('Resources/DNA_Synth.json', 'r') as file:
         data = json.load(file)
-        data['pattrstorage']['slots'][str(id)]['data']['noise_bank_gains'] = noise_bank_gains
-        data['pattrstorage']['slots'][str(id)]['data']['osc_bank_gains'] = osc_bank_gains
 
         for i in range(len(osc_freqs)):
             data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::A'] = [freq_curve_param[i][0]]
@@ -30,16 +28,16 @@ def write_params_to_json_file(id, length, osc_freqs, osc_bank_gains, noise_bank_
             data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::osc_freq'] = [osc_freqs[i]]
 
         for i in range(len(noise_bank_gains)):
-            data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::A'] = [noise_curve_param[i][0]]
-            data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::B'] = [noise_curve_param[i][1]]
-            data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::duration'] = [length]
-            data['pattrstorage']['slots'][str(id)]['data']['partial['+str(i+1)+']::static_gain'] = [noise_bank_gains[i]]
+            data['pattrstorage']['slots'][str(id)]['data']['noiseband['+str(i+1)+']::A'] = [noise_curve_param[i][0]]
+            data['pattrstorage']['slots'][str(id)]['data']['noiseband['+str(i+1)+']::B'] = [noise_curve_param[i][1]]
+            data['pattrstorage']['slots'][str(id)]['data']['noiseband['+str(i+1)+']::duration'] = [length]
+            data['pattrstorage']['slots'][str(id)]['data']['noiseband['+str(i+1)+']::static_gain'] = [noise_bank_gains[i]]
         
     ## Read from the JSON file DNASynth.json in Resources folder
     ## Update the values of the parameters
     ## Write the updated values to the same JSON file
     with open('Resources/curve_param.json', 'w') as outfile:
-        json.dump(data, outfile, indent=2)
+        json.dump(data, outfile, indent=2, sort_keys=True)
 
 
 if __name__=="__main__":
